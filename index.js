44c776303cbd599a22f91b0f304794a63507af38
Mehmet Catalbas <mehmet.catalbas@coda.xyz>
2021-07-24 03:04:21 +0300

    Sleuth deploy registration action

---
+++ index.js
@@ -0,0 +1,34 @@
+const core = require('@actions/core');
+const httpClient = require('@actions/http-client')
+
+async function main() {
+  try {
+    const organizationSlug = core.getInput('organizationSlug');
+    const deploymentSlug = core.getInput('deploymentSlug');
+    const environment = core.getInput('environment');
+    const email = core.getInput('email');
+    const apiKey = core.getInput('apiKey');
+    const sha = core.getInput('sha');
+
+    const requestUrl = `https://app.sleuth.io/api/1/${organizationSlug}/${deploymentSlug}/register_deploy`;
+    const data = {
+      api_key: apiKey,
+      sha: sha,
+      environment: environment,
+      email: email
+    };
+    
+    let response = await httpClient.post(requestUrl, data);
+
+    core.setOutput('status', response.message.statusCode);
+
+    let body = await response.readBody();
+    core.setOutput('body', body);
+
+  }
+  catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+main();
